53d88ff26e02cee9f2e503b3d581883db790f483
Roberto Moreno <robertomoreno@MacBook-Pro-de-Roberto.local>
2020-05-24 15:25:33 +0200

    Spring backend demo

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService { //In this class goes the business logic
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> selectAllPerson() {
+        return personDao.selectAllPerson();
+    }
+
+    public Optional<Person> selectPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePersonById(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePersonById(UUID id, Person person) {
+        return personDao.updatePersonById(id, person);
+    }
+}
